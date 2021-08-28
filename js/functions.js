3b042f2319ba1f476f512306885879b771e246b8
adrugdo <javiersanchezblanco@gmail.com>
2021-08-28 10:12:42 -0300

    nuevos cambios entrega

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,81 @@
+// Expresiones regulares
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	pass: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    CP:  /^\d{4,6}$/,
 	telefono: /^\d{9,11}$/ // 10 numeros.
 }
-
-const usuarios = [];
-
+// constructor del objeto
 class usuario {
-    constructor(nombre, pass, correo, telefono){
-        this.nombre = nombre;
-        this.pass = pass;
+    constructor(correo, nombre, apellido, localidad, CP, telefono){
         this.correo = correo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.localidad = localidad;
+        this.cp = cp;
         this.telefono = telefono;
     }
-    
-    nuevoNombre(){
-           this.nombre = prompt("ingrese nuevo nombre \n Debe contener letras solamente");
-            if(expresiones.nombre.test(this.nombre)){
-                this.nombre = this.nombre;
-                console.log("nombre true");
-            }
-            else {
-                console.log("nombre false");
-                this.nuevoNombre();
-            }
-        }
+}
+// Definicion de variables y constantes
+const usuarios = [];    
+
+let submit = document.getElementById("submit")
+let form = document.getElementById("form")
+let invalidnombre = document.getElementById("invalidNombre")
+
+// Funcion
+const nuevoUsuario = (e) => {
+    e.preventDefault()
+
+    let correo = document.getElementById("email").value
+    let nombre = validacionnombre()
+    let apellido = document.getElementById("apellido").value
+    let localidad = document.getElementById("localidad").value
+    let cp = document.getElementById("cp").value
+    let telefono = document.getElementById("telefono").value
+    console.log(`${correo} ${nombre} ${apellido} ${localidad}`)  
+}
+
+// validacion nombre y mensaje de error
+const validacionnombre = () => {
+if(expresiones.nombre.test(document.getElementById("email").value)){
+    this.nombre = document.getElementById("email").value;
+    console.log("nombre true");
+}
+else {
+    console.log("nombre false");
+    invalidnombre.classList.add('bg-danger')
+    invalidnombre.classList.add('badge')
+    invalidnombre.innerHTML = `Ingrese un nombre valido`
+}
+}
+
+// tomar datos
+form.addEventListener("submit",nuevoUsuario)
+
+
+/*
+
+class="badge bg-danger"
+
+
+
+*/
+
+
+/*
+
+let nombre2 = prompt("ingrese nuevo nombre \n Debe contener letras solamente");
+    if(expresiones.nombre.test(nombre2)){
+        this.nombre = nombre2;
+        console.log("nombre true");
+    }
+    else {
+        console.log("nombre false");
+        this.nuevoNombre();
+    }
+}
+
     nuevoPass(){
         this.pass = prompt("Ingrese una contraseña entre 4 y 12 digitos");
         if(expresiones.pass.test(this.pass)){
@@ -63,6 +112,7 @@ class usuario {
 
 }
 
+
 const usuario1 = new usuario("0","0","0","0");
 
 function guardarDatos(){
@@ -70,23 +120,16 @@ function guardarDatos(){
     usuarios.push(new usuario(usuario1));
 }
 
+
+  
 function suscripcion(){
 usuario1.nuevoNombre();
 usuario1.nuevoPass();
 usuario1.nuevoCorreo();
 usuario1.nuevoTelefono();
-console.log(usuario1)
+console.log(usuarios.toString())
 guardarDatos();
 console.log(usuarios.length)
 }
 
-do{
-suscripcion();
-}while(usuarios.length < 3);
-
-
-
-
-
-
-
+*/
